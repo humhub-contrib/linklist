2f4e398f4793da136426c36de7ad6dab193c325e
HumHub Translations <hello@humhub.com>
2023-03-01 18:06:39 +0000

    Enh: Updated Translations (translate.humhub.org)

--- messages/zh-TW/models_Category.php
+++ messages/zh-TW/models_Category.php
@@ -1,6 +1,6 @@
 <?php
 return array (
-  'Description' => '',
+  'Description' => '描述',
   'Sort Order' => '排列順序',
   'Title' => '標題',
 );
--- messages/zh-TW/models_Link.php
+++ messages/zh-TW/models_Link.php
@@ -1,7 +1,7 @@
 <?php
 return array (
   'Category' => '類別',
-  'Description' => '',
+  'Description' => '描述',
   'Sort Order' => '排列順序',
   'Title' => '標題',
 );
